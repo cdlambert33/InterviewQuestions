480a84e019f2a8a800d74826c1d757bc79fb7a47
Caston Lambert <cdlambert33@yahoo.com>
2022-11-10 11:33:50 -0600

    load data into timePunch list, convert start and end time strings to datetime objects

--- GeneralLogic/wage-calculator/classes.py
+++ GeneralLogic/wage-calculator/classes.py
@@ -7,12 +7,16 @@ class Job:
 
 
 class TimePunch:
-    def __init__(self, job, start, end):
+    def __init__(self, employee, job, start, end):
+        self.employee = employee
         self.job = job
         self.start = start
         self.end = end
 
 class Employee:
-    def __init__(self, employee, timePunch):
+    def __init__(self, id, employee, timeWorked, wageTotal, benefitTotal):
+        self.id = id
         self.employee = employee
-        self.timePunch = timePunch
+        self.timeWorked = timeWorked
+        self.wageTotal = wageTotal
+        self.benefitTotal = benefitTotal
--- GeneralLogic/wage-calculator/wage_calculator.py
+++ GeneralLogic/wage-calculator/wage_calculator.py
@@ -1,20 +1,48 @@
 import json
 from classes import Employee, Job, TimePunch
+from datetime import datetime, timedelta
 
-file = open('input.json')
 
-data = json.load(file)
+def main():
+    file = open('input.json')
 
-jobs = []
-employees = []
-timePunches = []
+    data = json.load(file)
 
-for i in data['jobMeta']:
-    job = Job(i.get('job'),i.get('rate'),i.get('benefitsRate'))
-    jobs.append(job)
+    jobs = []
+    employees = []
+    timePunches = []
 
-for i in data['employeeData']:
-    employee = Employee(i.get('employee'),i.get('timePunch'))
-    employees.append(employee)
+    for i in data['jobMeta']:
+        job = Job(i.get('job'), i.get('rate'), i.get('benefitsRate'))
+        jobs.append(job)
 
-file.close()
+    for i in data['employeeData']:
+        id = 0
+        employee = Employee(id, i.get('employee'), 0, 0, 0)
+        employees.append(employee)
+
+        for j in i['timePunch']:
+            start = datetime.strptime(j.get('start'), '%Y-%m-%d %H:%M:%S')
+            end = datetime.strptime(j.get('end'), '%Y-%m-%d %H:%M:%S')
+            punch = TimePunch(id, j.get('job'), start, end)
+            timePunches.append(punch)
+        
+        id += 1
+
+
+    for employee in employees:
+        for punch in timePunches:
+            getTime(punch.start, punch.end)
+            
+
+    file.close()
+
+
+def getTime(start,end):
+        difference = end - start
+        
+        return
+
+
+if __name__ == "__main__":
+    main()
